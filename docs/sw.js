35df5ceff05daaf748f1f82a1222904986ebbba4
yhorie <yuk.horie@gmail.com>
2017-03-25 21:53:30 +0900

    Convert PWA

--- docs/sw.js
+++ docs/sw.js
@@ -3,7 +3,7 @@ const cacheName = 'rf-0-0-1';
 self.addEventListener('install', function(e) {
   e.waitUntil(
     caches.open(cacheName).then(function(cache) {
-      console.log('[ServiceWorker] Caching app shell');
+      console.info('[ServiceWorker] Caching app shell');
       return cache.addAll([
         '/',
         '/index.html',
@@ -14,11 +14,11 @@ self.addEventListener('install', function(e) {
 });
 
 self.addEventListener('activate', function(e) {
-  console.log('[ServiceWorker] Activate');
+  console.info('[ServiceWorker] Activate');
   e.waitUntil(
     caches.keys().then(function(keyList) {
       return Promise.all(keyList.map(function(key) {
-        console.log('[ServiceWorker] Removing old cache', key);
+        console.info('[ServiceWorker] Removing old cache', key);
         if (key !== cacheName) {
           return caches.delete(key);
         }
@@ -28,7 +28,7 @@ self.addEventListener('activate', function(e) {
 });
 
 self.addEventListener('fetch', function(e) {
-  console.log('[ServiceWorker] Fetch', e.request.url);
+  console.info('[ServiceWorker] Fetch', e.request.url);
   e.respondWith(
     caches.match(e.request).then(function(response) {
       return response || fetch(e.request);
