d46e4f51415676d640f2dc237288899462020c9d
yhorie <yuk.horie@gmail.com>
2018-01-02 22:20:19 +0900

    Fix typo and delete console.log

--- docs/sw.js
+++ docs/sw.js
@@ -1,9 +1,8 @@
-const cacheName = 'rf-0-0-5';
+const cacheName = 'rf-0-0-6';
 
 self.addEventListener('install', function(e) {
   e.waitUntil(
     caches.open(cacheName).then(function(cache) {
-      console.info('[ServiceWorker] Caching app shell');
       return cache.addAll([
         './',
         './index.html',
@@ -18,11 +17,9 @@ self.addEventListener('install', function(e) {
 });
 
 self.addEventListener('activate', function(e) {
-  console.info('[ServiceWorker] Activate');
   e.waitUntil(
     caches.keys().then(function(keyList) {
       return Promise.all(keyList.map(function(key) {
-        console.info('[ServiceWorker] Removing old cache', key);
         if (key !== cacheName) {
           return caches.delete(key);
         }
@@ -32,7 +29,6 @@ self.addEventListener('activate', function(e) {
 });
 
 self.addEventListener('fetch', function(e) {
-  console.info('[ServiceWorker] Fetch', e.request.url);
   e.respondWith(
     caches.match(e.request).then(function(response) {
       return response || fetch(e.request);
