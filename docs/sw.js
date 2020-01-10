bf1c748e66612a155b5b290b4bcd921806a7fbb5
yhorie <yuk.horie@gmail.com>
2020-01-10 13:46:26 +0900

    Workbox

--- docs/sw.js
+++ docs/sw.js
@@ -1,46 +1,6 @@
-//const cacheName = 'rf-0-0-6';
-//
-//self.addEventListener('install', function(e) {
-//  e.waitUntil(
-//    caches.open(cacheName).then(function(cache) {
-//      return cache.addAll([
-//        './',
-//        './index.html',
-//        './main.js',
-//        './material-icons.css',
-//        './material.blue_grey-blue.min.css',
-//        './material.min.js',
-//        './MaterialIcons-Regular.woff2'
-//      ]);
-//    })
-//  );
-//});
-//
-//self.addEventListener('activate', function(e) {
-//  e.waitUntil(
-//    caches.keys().then(function(keyList) {
-//      return Promise.all(keyList.map(function(key) {
-//        if (key !== cacheName) {
-//          return caches.delete(key);
-//        }
-//      }));
-//    })
-//  );
-//});
-//
-//self.addEventListener('fetch', function(e) {
-//  e.respondWith(
-//    caches.match(e.request).then(function(response) {
-//      return response || fetch(e.request);
-//    })
-//  );
-//});
-
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.5.0/workbox-sw.js');
-
 if (workbox) {
-  console.log(`Yay! Workbox is loaded`);
-
+  console.log(`Workbox is loaded`);
   workbox.precaching.precacheAndRoute([
   {
     "url": "index.html",
@@ -71,7 +31,6 @@ if (workbox) {
     "revision": "6276f3607b19efba8d706d70a232cc43"
   }
 ]);
-
 } else {
-  console.log(`Boo! Workbox didn't load`);
+  console.log(`Workbox didn't load`);
 }
--- src/sw.js
+++ src/sw.js
@@ -1,48 +1,7 @@
-//const cacheName = 'rf-0-0-6';
-//
-//self.addEventListener('install', function(e) {
-//  e.waitUntil(
-//    caches.open(cacheName).then(function(cache) {
-//      return cache.addAll([
-//        './',
-//        './index.html',
-//        './main.js',
-//        './material-icons.css',
-//        './material.blue_grey-blue.min.css',
-//        './material.min.js',
-//        './MaterialIcons-Regular.woff2'
-//      ]);
-//    })
-//  );
-//});
-//
-//self.addEventListener('activate', function(e) {
-//  e.waitUntil(
-//    caches.keys().then(function(keyList) {
-//      return Promise.all(keyList.map(function(key) {
-//        if (key !== cacheName) {
-//          return caches.delete(key);
-//        }
-//      }));
-//    })
-//  );
-//});
-//
-//self.addEventListener('fetch', function(e) {
-//  e.respondWith(
-//    caches.match(e.request).then(function(response) {
-//      return response || fetch(e.request);
-//    })
-//  );
-//});
-
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.5.0/workbox-sw.js');
-
 if (workbox) {
-  console.log(`Yay! Workbox is loaded`);
-
+  console.log(`Workbox is loaded`);
   workbox.precaching.precacheAndRoute([]);
-
 } else {
-  console.log(`Boo! Workbox didn't load`);
+  console.log(`Workbox didn't load`);
 }
